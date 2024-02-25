bd1e6d6575a69f355f3972cfc5ff9a316a617e32
Sanvaad <96122726+Sanvaad@users.noreply.github.com>
2024-02-25 11:33:04 +0530

    help audio

---
+++ Hero.jsx
@@ -0,0 +1,20 @@
+function Hero() {
+  return (
+    <div className="hero-container">
+      <div className="hero">
+        <div className="hero-content-container">
+          <h1 className="hero__product--arrival">NEW PRODUCT</h1>
+          <h2 className="hero__product--name">XX99 MARK II HEADPHONES</h2>
+          <small className="hero__product--description">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </small>
+
+          <button className="btn btn--primary">SEE PRODUCT</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
